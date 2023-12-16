55e11939079d23c4630b79ac04068bfdeac2b087
shohaib1996 <khanshohaibhossain@gmail.com>
2023-12-16 23:16:43 +0600

    problem one is solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,25 +1,71 @@
-import React, {useState} from 'react';
+import  { useState } from 'react';
 
 const Problem1 = () => {
-
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState({ name: '', status: '' });
     const [show, setShow] = useState('all');
 
-    const handleClick = (val) =>{
-        setShow(val);
-    }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewTask({ ...newTask, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (newTask.name && newTask.status) {
+            setTasks([...tasks, newTask]);
+            setNewTask({ name: '', status: '' }); 
+        }
+    };
+
+    const handleClick = (status) => {
+        setShow(status);
+    };
+
+    const getFilteredTasks = () => {
+        if (show === 'active') {
+            return tasks.filter(task => task.status.toLowerCase() === 'active');
+        } else if (show === 'completed') {
+            return tasks.filter(task => task.status.toLowerCase() === 'completed');
+        } else {
+            return tasks.sort((a, b) => {
+                if (a.status.toLowerCase() === 'active') return -1;
+                if (b.status.toLowerCase() === 'active') return 1;
+                if (a.status.toLowerCase() === 'completed') return -1;
+                if (b.status.toLowerCase() === 'completed') return 1;
+                return 0;
+            });
+        }
+    };
+    
+
+    const filteredTasks = getFilteredTasks();
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleSubmit}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                name="name"
+                                value={newTask.name}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Status"
+                                name="status"
+                                value={newTask.status}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,27 +75,33 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' ? 'active' : ''}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' ? 'active' : ''}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' ? 'active' : ''}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
+                    <div className="tab-content">
+                        <table className="table table-striped ">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredTasks.map((task, index) => (
+                                    <tr key={index}>
+                                        <td>{task.name}</td>
+                                        <td>{task.status}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
